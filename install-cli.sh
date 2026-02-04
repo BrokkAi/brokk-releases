85fcfe465e329c83a98f268d959708ede6ab3096
Jonathan Ellis <jbellis@gmail.com>
2026-02-04 10:27:52 +0100

    0.21.0.2

--- install-cli.sh
+++ install-cli.sh
@@ -2,11 +2,15 @@
 
 set -e
 
-JBANG_DEFAULT_JAVA_VERSION=21 curl -fsSL https://sh.jbang.dev | bash -s - app setup
+echo "Installing JBang bootstrapper"
+curl -fsSL https://sh.jbang.dev | JBANG_DEFAULT_JAVA_VERSION=21 bash -s - app setup --quiet
 export PATH="$HOME/.jbang/bin:$PATH"
 
+echo "Installing Brokk CLI"
 jbang trust add https://github.com/BrokkAi/brokk-releases
 jbang app install brokk@brokkai/brokk-releases
 
 brokk login $userkey
 brokk install
+
+echo "Brokk is installed and on your PATH but you will need to open a new shell for it to take effect."
